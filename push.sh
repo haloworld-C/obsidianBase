364d605cf4e9d7186121f6a966a430feab3387a8
haloworld-C <guangaltman@163.com>
2021-12-05 13:39:46 +0800

    2021-12-05

--- push.sh
+++ push.sh
@@ -1,7 +1,6 @@
+#! /bin/bash
 # update throuht git tool by halo
-now = $(date "+%Y-%m-%d %H:%M:%S")
-# comments = 
-git add .
-git commit -m "20211122noon" 
-git pull origin main
-git push origin main
+echo "begin to push current repo.."
+now=$(date "+%Y-%m-%d")
+git add -A && git commit -m "$now" && git pull origin main && git push origin main
+echo "git push finished!"
