948f86b85bdc4fda48f132ffe7263f62ffb32088
haloworld-C <guangaltman@163.com>
2021-12-05 13:45:54 +0800

    2021-12-05-afernoon

--- push.sh
+++ push.sh
@@ -3,5 +3,5 @@
 echo "begin to push current repo.."
 now=$(date "+%Y-%m-%d")
 read -p "please input commit comments:" msg
-git add -A && git commit -m "$msg"+"-"+"$now" && git pull origin main && git push origin main
+git add -A && git commit -m "$now"-"$msg" && git pull origin main && git push origin main
 echo "git push finished!"
